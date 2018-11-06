0de879c1a8e9cd903744591ca98af401fc71db19
atomiks <cc.glows@gmail.com>
2018-11-07 04:00:34 +1100

    Add ESM file

--- build.js
+++ build.js
@@ -1,19 +1,37 @@
-import babel from 'rollup-plugin-babel'
-import minify from 'rollup-plugin-babel-minify'
+const { rollup } = require('rollup')
+const babel = require('rollup-plugin-babel')
+const minify = require('rollup-plugin-babel-minify')
 
-export default {
-  input: './elasticscroll.js',
-  output: {
-    file: './dist/elasticscroll.js',
-    format: 'umd',
-    name: 'elasticScroll'
-  },
-  externalHelpers: true,
-  plugins: [
-    babel({
-      presets: ['es2015-rollup'],
-      plugins: ['transform-object-assign', 'transform-object-rest-spread']
-    }),
-    minify({ comments: false })
-  ]
+const pluginBabel = babel({
+  babelrc: false,
+  exclude: 'node_modules/**',
+  presets: ['@babel/preset-env'],
+  plugins: [['@babel/plugin-proposal-object-rest-spread', { loose: true }]]
+})
+const pluginMinify = minify({ comments: false })
+
+const rollupConfig = (...plugins) => ({
+  input: './src/index.js',
+  plugins: [pluginBabel, ...plugins]
+})
+const output = format => file => ({
+  name: 'elasticScroll',
+  format,
+  file,
+  sourcemap: true
+})
+
+const umd = output('umd')
+const esm = output('es')
+
+const build = async () => {
+  const bundle = await rollup(rollupConfig())
+  const bundleMin = await rollup(rollupConfig(pluginMinify))
+
+  bundle.write(umd('./dist/index.js'))
+  bundleMin.write(umd('./dist/index.min.js'))
+  bundle.write(esm('./dist/esm/index.js'))
+  bundleMin.write(esm('./dist/esm/index.min.js'))
 }
+
+build()
